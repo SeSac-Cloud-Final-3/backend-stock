6c21779f94ebdeb644e8a4bcf768fc266b217f6d
SeSAC-Cloud-CTW <slowlight50@gmail.com>
2022-04-28 17:54:01 +0900

    Import library

--- src/main/java/monkey/config/auth/JwtAuthenticationFilter.java
+++ src/main/java/monkey/config/auth/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package monkey.config.auth;
 
 import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -12,6 +13,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
